44265c31267b1c94bfd30e58b479ec5d3595ce00
Sverre H. Huseby <sverre.huseby@statnett.no>
2026-09-02 13:24:13 +0200

    foo

--- component/servo.go
+++ component/servo.go
@@ -1,6 +1,7 @@
 package component
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/sverrehu/rpigo/gpio"
@@ -16,23 +17,34 @@ type Servo struct {
 	PWM             *gpio.SoftPWM
 }
 
-func NewServo(pin *Pin, frequency float64) (*Servo, error) {
+func NewServo(pin *Pin) (*Servo, error) {
+	// Matches several servos, including SG92R
+	return NewServoWithSpec(pin, 500*time.Microsecond, 2500*time.Microsecond, 180, 50)
+}
+
+func NewServoWithSpec(pin *Pin, pulseControlMin, pulseControlMax time.Duration, maxAngle int, frequency float64) (*Servo, error) {
 	c, err := NewOutputComponent(pin)
 	if err != nil {
 		return nil, err
 	}
 	s := &Servo{
 		OutputComponent: *c,
-		PulseControlMin: time.Duration(500 * time.Microsecond),
-		PulseControlMax: time.Duration(2500 * time.Microsecond),
-		MaxAngle:        180,
+		PulseControlMin: pulseControlMin,
+		PulseControlMax: pulseControlMax,
+		MaxAngle:        maxAngle,
 		PWM:             gpio.NewSoftPWM(pin.line, frequency),
 	}
 	return s, nil
 }
 
-func (s *Servo) Set(b bool) error {
-	return s.Pin.Set(b)
+func (s *Servo) SetAngle(angle float64) error {
+	if angle < 0 || angle > float64(s.MaxAngle) {
+		return fmt.Errorf("angle must be between 0 and %d inclusive", s.MaxAngle)
+	}
+	// TODO: calculate duty cycle
+	dutyCycle := 0
+	s.PWM.SetDutyCycle(dutyCycle)
+	return nil
 }
 
 func (s *Servo) Close() error {
